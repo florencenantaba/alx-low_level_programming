2c3845a1d7e3043fdde674576f60faedea4900e6
florencenantaba <tendoprecious38@gmail.com>
2022-04-05 09:48:03 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,73 +1,117 @@
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
+#include <stdbool.h>
+
+#include "main.h"
+
+
 
 /**
- * check_num - check - string there are digit
- * @str: array str
+
+ * is_num - iterate through each argv to test if it's a number
+
+ * @argvv: A argv
+
  *
- * Return; Always 0 (Success)
+
+ * Return: true only if entire string is a number else false
+
  */
 
-int check_num(char *str)
+
+
+bool is_num(char *argvv)
 
 {
-/*Declaring variables*/
-unsigned int count;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
+	int j = 0;
 
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
+
+
+	for (j = 0; argvv[j]; j++)
+
+	{
+
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+
+			return (0);
+
+	}
+
+
+
+	return (1);
+
 }
 
-count++;
-}
-return (1);
-}
+
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+
+ * main - print sum if all arguments given are numbers
+
+ * @argc: argument counter
+
+ * @argv: arguments
+
  *
- * return: Always 0 (Success)
+
+ * Return: 0 as success
+
  */
+
+
 
 int main(int argc, char *argv[])
 
 {
 
-/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
+	int i = 0;
 
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if(check_num(argv[count]))
+	int sum = 0;
 
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-sum += str_to_int;
-}
 
-/*Condition if one of the number contains symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
 
-count++;
-}
+	if (argc == 1)
 
-printf("%d\n", sum); /*print sum*/
+	{
 
-return (0);
+		printf("0\n");
+
+		return (0);
+
+	}
+
+
+
+	while (i < argc)
+
+	{
+
+		if (is_num(argv[i]))
+
+			sum += atoi(argv[i]);
+
+		else
+
+		{
+
+			printf("Error\n");
+
+			return (1);
+
+		}
+
+		i++;
+
+	}
+
+
+
+	printf("%d\n", sum);
+
+	return (0);
+
 }
