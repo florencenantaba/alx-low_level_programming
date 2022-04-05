cf61bea822bf95244ebfd8602f80359844fde9b2
florencenantaba <tendoprecious38@gmail.com>
2022-04-05 09:57:00 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <ctype.h>
-
 /**
-
  * main - adds positive numbers.
-
  * @argc: number of command line arguments.
-
  * @argv: array that contains the program command line arguments.
-
  * Return: 0 - success.
-
  */
 
 int main(int argc, char *argv[])
